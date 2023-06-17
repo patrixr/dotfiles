b9bcd1e246493fb87da29a1610f02a76ba429fc5
Patrick <patrick@Patricks-MacBook-Air.local>
2023-06-17 13:16:31 +0800

    FIX: general structure

---
+++ dependencies.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+echo "=== Installing HomeBrew"
+
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "=== Setting up .zprofile with Homebrew information"
+
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/patrick/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+
+echo "=== Installing zsh and ansible with homebrew"
+
+brew install zsh ansible
+
+echo "=== Installing Oh My Zsh"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "=== Restoring backed-up zsh configurations (.zshrc)"
+
+cat ./backups/zshrc-addons >> ~/.zshrc
+
+echo "=== Installing ansible collections needed by the playbook"
+
+ansible-galaxy collection install community.general
