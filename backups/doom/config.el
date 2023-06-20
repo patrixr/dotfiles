2ef33ad58722dec120082cd8dd2819cc6bed25c3
Patrick R <1822532+patrixr@users.noreply.github.com>
2023-06-20 12:25:48 +0800

    Update config.el

--- backups/doom/config.el
+++ backups/doom/config.el
@@ -3,6 +3,7 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+(define-key global-map (kbd "M-p") 'project-find-file)
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
