d1d4b5338f41e1bd7d27716bf844b6d16af0bf6c
Patrick R <1822532+patrixr@users.noreply.github.com>
2023-06-21 09:48:47 +0800

    Update packages.el

--- backups/doom/packages.el
+++ backups/doom/packages.el
@@ -6,6 +6,7 @@
 ;; use 'M-x doom/reload'.
 
 ;;(package! terraform-mode)
+(package! prettier-js)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
