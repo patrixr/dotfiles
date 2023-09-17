981ec743a7794815d914a399f8a80c533129a279
Patrick <1822532+patrixr@users.noreply.github.com>
2023-09-17 09:54:16 +0800

    Add nova cask

--- configs/doom/packages.el
+++ configs/doom/packages.el
@@ -9,6 +9,7 @@
 (package! prettier-js)
 (package! feature-mode) ;; cucumber support
 
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
