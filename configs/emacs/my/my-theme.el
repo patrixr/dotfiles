a827ba7bf1ceef63f0a718ab02ea915b63420215
Patrick <1822532+patrixr@users.noreply.github.com>
2023-12-22 18:02:47 +0800

    Change theme

--- configs/emacs/my/my-theme.el
+++ configs/emacs/my/my-theme.el
@@ -3,7 +3,7 @@
   :config
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one-light t)
+  (load-theme 'doom-challenger-deep t)
   (doom-themes-visual-bell-config) ; Enable flashing mode-line on errors
   (doom-themes-neotree-config) ; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-org-config)) ; Corrects (and improves) org-mode's native fontification.
